afb162e1c1c63fe9e372e4850cc4fea3688e3ee4
ewroldsen <ewroldsen@gmail.com>
2024-04-18 18:10:58 -0400

    add endpoints

--- Program.cs
+++ Program.cs
@@ -21,14 +21,64 @@ app.UseHttpsRedirection();
 
 
 async Task<List<Court>> GetCourts(DataContext context) => await context.Courts.ToListAsync();
+
+/// Get courts
+app.MapGet("/courts", async (DataContext context) => await context.Courts.ToListAsync())
+    .WithName("getCourt")
+   .WithOpenApi();
+
+/// Get courts by id
+app.MapGet("/courts/{id}", async (DataContext context, int id) => await context.Courts.FindAsync(id) is Court court ? Results.Ok(court) : Results.NotFound("Court not found"))
+    .WithName("getCourtbyid")
+   .WithOpenApi();
+
+/// Add courts
 app.MapPost("add/court", async (DataContext context, Court court) =>
 {
    context.Courts.Add(court);
    await context.SaveChangesAsync();
    return Results.Ok(await GetCourts(context));
-});
+})
+   .WithName("AddCourt")
+   .WithOpenApi();
 
-app.MapGet("/courts", async (DataContext context) => await context.Courts.ToListAsync());
+/// Update court
+app.MapPut("update/court{id}", async (DataContext context, Court court, int id) =>
+{
+   var courtToUpdate = await context.Courts.FindAsync(id);
+   if (courtToUpdate == null) return Results.NotFound("Court not found");
+   courtToUpdate.Id = id;
+   courtToUpdate.CourtType = court.CourtType;
+   courtToUpdate.NumbOfCourts = court.NumbOfCourts;
+   courtToUpdate.Title = court.Title;
+   courtToUpdate.StreetAddress = court.StreetAddress;
+   courtToUpdate.City = court.City;
+   courtToUpdate.StateAbrev = court.StateAbrev;
+   courtToUpdate.ZipCode = court.ZipCode;
+   courtToUpdate.Latitude = court.Latitude;
+   courtToUpdate.Longitude = court.Longitude;
+   context.Courts.Update(courtToUpdate);
+   await context.SaveChangesAsync();
+   return Results.Ok(await GetCourts(context));
+})
+    .WithName("updateCourt")
+   .WithOpenApi();
+
+/// Delete court
+
+app.MapDelete("delete/court{id}", async (DataContext context, int id) =>
+{
+   var courtToRemove = await context.Courts.FindAsync(id);
+   if (courtToRemove == null) return Results.NotFound("Court not found");
+
+   context.Remove(courtToRemove);
+   await context.SaveChangesAsync();
+   return Results.Ok(await GetCourts(context));
+})
+    .WithName("Delete-Court")
+   .WithOpenApi();
+
+
 
 
 app.Run();
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# demo.WebApiDotNetCore8.Minimal
+.NET Core 8 Web Minimal APi
+
+Basic Minimal API CRUD Operations.
